0ba3caba6ebb7bfff7b1632e1fc11eb516dc8433
bic-L <yuetong.fyt@gmail.com>
2025-10-19 17:54:57 +0800

    intial commit

--- imagenet/checkpoints/download.sh
+++ imagenet/checkpoints/download.sh
@@ -3,3 +3,5 @@ wget https://github.com/bic-L/MaxFormer/releases/download/checkpoints/10-512-T1.
 wget https://github.com/bic-L/MaxFormer/releases/download/checkpoints/10-512-T4.pth.tar
 wget https://github.com/bic-L/MaxFormer/releases/download/checkpoints/10-768-T1.pth.tar
 wget https://github.com/bic-L/MaxFormer/releases/download/checkpoints/10-768-T4.pth.tar
+wget https://github.com/bic-L/MaxFormer/releases/download/checkpoints/ms_qk_10-384_T4.pth.tar
+wget https://github.com/bic-L/MaxFormer/releases/download/checkpoints/ms_qk_10-768_T1.pth.tar
